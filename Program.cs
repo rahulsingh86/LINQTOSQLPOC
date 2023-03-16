8456f56f0c1313ce6603f59fb39614e16d31a9b3
rahulsingh86 <rahulsingh86@users.noreply.github.com>
2023-03-16 11:54:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityToSQL
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notes obj = new Notes( );
+            
+            obj.CATEGORY = "First Category";
+            obj.SUBJECT = "Subject of the Category";
+            obj.NODETEXT = "Sample text of the Node";
+
+            Entity<Notes> entity = new Entity<Notes> ();
+            entity.Add(obj);
+
+            // Code to get the object values
+            Notes note = entity.Get(typeof(System.String), "CATEGORY", "First Category");
+        }
+    }
+}
