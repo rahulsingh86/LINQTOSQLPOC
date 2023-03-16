ee659a4ea7fa4e2de1ae5d4ef415144e1978b52f
APAC\rahsin01 <rahulsinghrathore86@gmail.com>
2023-03-16 13:05:26 +0530

    no message

---
+++ EntityToSQL/IEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityToSQL
+{
+    interface IEntity<T> where T : class
+    {
+        IList<T> List();
+
+        IList<T> List(int? page, int? pageSize, Expression<Func<T, bool>> predicate, Expression<Func<T, object>> sort);
+
+        void Add(T item);
+
+        T Get(Int64 Id);
+
+        void Update(T item);
+
+        bool Delete(T Item);
+    }
+}
---
+++ EntityToSQL/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityToSQL
+{
+	[Table(Name = "dbo.Notes")]
+	public class Notes
+    {
+		[Column(IsPrimaryKey = true)]
+		public string CATEGORY { get; set; }
+		[Column]
+		public string SUBJECT { get; set; }
+		[Column]
+		public string NODETEXT { get; set; }
+	}
+}
